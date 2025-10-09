dfe437958e3b9cf48e810097f7ad0af4447ca8d0
insharahkalam <insharahkalam19@gmail.com>
2025-10-10 04:03:24 +0500

    Setup frontend axios base URL for dev and production

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,19 @@
+// import axios from "axios";
+
+// const BASE_URL = import.meta.env.MODE === "development" ? "https://thinkboard-backend-gold.vercel.app/api/notes" : "/api"
+
+// const api = axios.create({
+//     baseURL: BASE_URL
+// })
+
+// export default api;
+
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
+const BASE_URL = import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api/notes"       // local backend
+    : "https://thinkboard-backend-gold.vercel.app/api/notes"; // live backend
 
 const api = axios.create({
     baseURL: BASE_URL
