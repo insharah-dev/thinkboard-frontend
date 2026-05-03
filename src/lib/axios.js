c4898bb9c50f48198299a446b046b96d0d0912d0
insharahkalam <insharahkalam19@gmail.com>
2026-05-03 22:24:30 +0500

    updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -11,9 +11,13 @@
 
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development"
-    ? "http://localhost:3000/api/notes"  
-    : "https://thinkboard-backend-gold.vercel.app/api/notes"; 
+// const BASE_URL = import.meta.env.MODE === "development"
+//     ? "http://localhost:3000/api/notes"
+//     : "https://thinkboard-backend-gold.vercel.app/";
+
+
+const BASE_URL = "http://localhost:3000/api/notes" || "https://thinkboard-backend-gold.vercel.app/";
+
 
 const api = axios.create({
     baseURL: BASE_URL
