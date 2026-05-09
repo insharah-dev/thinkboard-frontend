9352660802512a21bd34ccb558b21bc70f57aec8
insharahkalam <insharahkalam19@gmail.com>
2026-05-10 00:47:42 +0500

    axios fix

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/notes" || "https://thinkboard-backend-gold.vercel.app";
+const BASE_URL = process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api/notes"
+    : "https://thinkboard-backend-gold.vercel.app/api/notes";
 
 const api = axios.create({
-    baseURL: BASE_URL
-})
+    baseURL: BASE_URL,
+});
 
 export default api;
