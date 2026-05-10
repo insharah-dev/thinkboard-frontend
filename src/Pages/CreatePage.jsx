42989c46ecea1a0f3ce72e5de5b0783ab84a8eb5
insharahkalam <insharahkalam19@gmail.com>
2026-05-10 09:36:32 +0500

    updated routes

--- src/Pages/CreatePage.jsx
+++ src/Pages/CreatePage.jsx
@@ -23,7 +23,7 @@ const CreatePage = () => {
         title, content
       })
       toast.success("Note Created Successfully!")
-      navigate("/")
+      navigate("/getAll")
     } catch (error) {
       console.log("Error creating note", error);
       if (error.response.status === 429) {
@@ -43,7 +43,7 @@ const CreatePage = () => {
     <div className='min-h-screen bg-base-200'>
       <div className='container mx-auto px-4 py-8'>
         <div className='max-w-2xl mx-auto'>
-          <Link to={'/'} className='btn btn-ghost mb-6'>
+          <Link to={'/getAll'} className='btn btn-ghost mb-6'>
             <ArrowLeftIcon className="size-5" />
             Back to Notes
           </Link>
